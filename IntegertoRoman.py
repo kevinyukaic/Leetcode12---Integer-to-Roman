bed1417a4b36e3e153120a8b761ad0fe0b82b2f7
kevinyukaic <yukaic@andrew.cmu.edu>
2019-03-23 18:41:02 -0400

    Update IntegertoRoman.py
    Add int solution

--- IntegertoRoman.py
+++ IntegertoRoman.py
@@ -1,7 +1,9 @@
+#Method 1: transfer to string to handle
 class Solution:
     def intToRoman(self, num: int) -> str:
         #Guaranteed to be within the range of 1 to 3999
         #no need to perform edge check here
+        
         
         Result = ""
         Roman_Digit = {1:"I",2:"V",3:"X",4:"L",5:"C",6:"D",7:"M"}
@@ -23,6 +25,36 @@ class Solution:
                 
             num = num[:-1]
             RomanP+=2
+            
+        return Result
+
+        
+#Method 2: Pure int solution (Faster)
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        #Guaranteed to be within the range of 1 to 3999
+        #no need to perform edge check here
+        
+        Result = ""
+        Roman_Digit = {1:"I",5:"V",10:"X",50:"L",100:"C",500:"D",1000:"M"}
+        RomanP = 1
+        #num = str(num)
+        while(num):
+            num, remainder = divmod(num,10)
+            
+            #Case: 4 or 9
+            if(remainder == 4):
+                Result = Roman_Digit[RomanP] + Roman_Digit[RomanP*5] + Result
+            elif(remainder == 9):
+                Result = Roman_Digit[RomanP] + Roman_Digit[RomanP*10] + Result
+            #Case: others
+            else:
+                if(remainder>=5):
+                    Result = Roman_Digit[RomanP*5] + Roman_Digit[RomanP] * (remainder - 5) + Result
+                else:
+                    Result = Roman_Digit[RomanP] * remainder + Result                
+            
+            RomanP*=10
             
         return Result
         
